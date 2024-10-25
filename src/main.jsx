9abc128122345056be2c6bec2d87c23e4e10890b
Lerish-vZ <lerishavz@gmail.com>
2024-10-25 11:16:02 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
 
-ReactDom.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
+  </BrowserRouter>
 );
