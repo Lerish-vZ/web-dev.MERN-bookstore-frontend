45133a401950b10907db66cf4ac78d1e583dadad
Lerish-vZ <lerishavz@gmail.com>
2024-11-26 11:04:13 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-    server: {
-      host: '0.0.0.0', // Expose server to local network
-      port: 5173, // Explicitly set port if necessary
-    },
-  
+
 })
